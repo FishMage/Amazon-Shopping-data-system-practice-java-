386edb906de667018c979fa2561a4375b9b578dc
FishMage <siyu.194@gmail.com>
2016-03-03 14:20:25 -0600

    Update AmazonStore.java

--- AmazonStore.java
+++ AmazonStore.java
@@ -1,44 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
-//                   ALL STUDENTS COMPLETE THESE SECTIONS
-// Title:            CS 367_p2
-// Files:            AmazonStore.java; DLinkedList.java; User.Java; Product.java;
-//					 InsufficientCreditException.java
-// Semester:         CS367 Spring 2015
-//
-// Author:           Richard Chen (Siyu)
-// Email:            schen358@wisc.edu
-// CS Login:         chens
-// Lecturer's Name:  Jim Skrentny
-// 
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
-//
-//                   CHECK ASSIGNMENT PAGE TO see IF PAIR-PROGRAMMING IS ALLOWED
-//                   If pair programming is allowed:
-//                   1. Read PAIR-PROGRAMMING policy (in cs302 policy) 
-//                   2. choose a partner wisely
-//                   3. REGISTER THE TEAM BEFORE YOU WORK TOGETHER 
-//                      a. one partner creates the team
-//                      b. the other partner must join the team
-//                   4. complete this section for each program file.
-//
-// Pair Partner:     Zixin Chi
-// Email:            zchi4@wisc.edu
-// CS Login:         zixin
-// Lecturer's Name:  Jim Skrentny
-//
-//////////////////// STUDENTS WHO GET HELP FROM OTHER THAN THEIR PARTNER //////
-//                   must fully acknowledge and credit those sources of help.
-//                   Instructors and TAs do not have to be credited here,
-//                   but tutors, roommates, relatives, strangers, etc do.
-//
-// Persons:          Identify persons by name, relationship to you, and email.
-//                   Describe in detail the the ideas and help they provided.
-//
-// Online sources:   avoid web searches to solve your problems, but if you do
-//                   search, be sure to include Web URLs and description of 
-//                   of any information you find.
-//////////////////////////// 80 columns wide //////////////////////////////////
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
